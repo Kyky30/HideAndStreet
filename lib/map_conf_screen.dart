4605ec277f68dbf2b55e2664dcb2d9fe02abdf55
Dorian Rochette <dorian.rochette@etu.uca.fr>
2024-01-09 17:45:08 +0100

    transition de la map vers flutter_map la map s'affiche

--- lib/map_conf_screen.dart
+++ lib/map_conf_screen.dart
@@ -1,5 +1,7 @@
 // map_conf_screen.dart
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
 
 class MapConfScreen extends StatelessWidget {
   @override
@@ -8,9 +10,25 @@ class MapConfScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Map Configuration'),
       ),
-      body: const Center(
-        child: Text('Contenu de la configuration de la carte'),
-      ),
+      body: FlutterMap(
+        options: const MapOptions(
+          initialCenter: LatLng(51.509364, -0.128928),
+          initialZoom: 9.2,
+        ),
+        children: [
+          TileLayer(
+            urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+            userAgentPackageName: 'com.example.app',
+          ),
+          const RichAttributionWidget(
+            attributions: [
+              TextSourceAttribution(
+                'OpenStreetMap contributors',
+              ),
+            ],
+          ),
+        ],
+      )
     );
   }
 }
