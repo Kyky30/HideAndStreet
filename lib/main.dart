46af708d0856108a41f4c5c23bdf10687070f38a
Julien ANGENIEUX <julien.angenieux@gmail.com>
2024-01-09 14:50:46 +0100

    Vla Huge Home UI Improvement
    - Ajout des packages de squircle (figma-squircle)
    - Ajout des packages d'icones de google
    - Modfification des icones
    - Modification des boutons (coleurs, taille, textes et formes)
    - Traductionde la navbar en français (account -> Compte)
    - Ajout de l'image d'arrière plan
    - Ajout des polices et de leurs graisse correspondante (Poppins)

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 // main.dart
 import 'package:flutter/material.dart';
+
 import 'package:hide_and_street/home.dart';
 import 'package:hide_and_street/shop.dart';
 import 'package:hide_and_street/account_settings.dart';
+
+import 'package:material_symbols_icons/symbols.dart';
+import 'package:material_symbols_icons/material_symbols_icons.dart';
+
 
 void main() {
   runApp(MyApp());
@@ -15,6 +20,10 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Bottom Navigation',
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        iconTheme: const IconThemeData(color: Colors.black,
+            fill: 1,
+            weight: 700,
+            opticalSize: 24),
       ),
       home: MyHomePage(),
     );
@@ -48,16 +57,16 @@ class _MyHomePageState extends State<MyHomePage> {
       bottomNavigationBar: BottomNavigationBar(
         items: [
           BottomNavigationBarItem(
-            icon: Icon(Icons.store),
-            label: 'Shop',
+            icon: Icon(Symbols.shopping_cart_rounded, fill: 1, weight: 700, grade: 200, opticalSize: 24 ),
+            label: 'Boutique',
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
+            icon: Icon( Symbols.home_rounded, fill: 1, weight: 700, grade: 200, opticalSize: 24 ),
+            label: 'Menu',
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.account_circle),
-            label: 'Account',
+            icon: Icon(Symbols.settings_account_box_rounded,fill: 1, weight: 700, grade: 200, opticalSize: 24 ),
+            label: 'Compte',
           ),
         ],
         currentIndex: _selectedIndex,
