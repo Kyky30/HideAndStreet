6053cd31d19256bec106fa403a5161364cb6d3a4
Dorian Rochette <dorian.rochette@etu.uca.fr>
2024-01-09 18:46:50 +0100

    Ajout de la localisation de l'utilisateur a fin de l'afficher de maniére à afficher la map centré

--- lib/map_conf_screen.dart
+++ lib/map_conf_screen.dart
@@ -1,34 +1,94 @@
 // map_conf_screen.dart
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:latlong2/latlong.dart';
 
 class MapConfScreen extends StatelessWidget {
+
+  /// Determine the current position of the device.
+  ///
+  /// When the location services are not enabled or permissions
+  /// are denied the `Future` will return an error.
+  Future<Position> _determinePosition() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    // Test if location services are enabled.
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      // Location services are not enabled don't continue
+      // accessing the position and request users of the
+      // App to enable the location services.
+      return Future.error('Location services are disabled.');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        // Permissions are denied, next time you could try
+        // requesting permissions again (this is also where
+        // Android's shouldShowRequestPermissionRationale
+        // returned true. According to Android guidelines
+        // your App should show an explanatory UI now.
+        return Future.error('Location permissions are denied');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      // Permissions are denied forever, handle appropriately.
+      return Future.error(
+          'Location permissions are permanently denied, we cannot request permissions.');
+    }
+
+    // When we reach here, permissions are granted and we can
+    // continue accessing the position of the device.
+    return await Geolocator.getCurrentPosition();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Map Configuration'),
-      ),
-      body: FlutterMap(
-        options: const MapOptions(
-          initialCenter: LatLng(51.509364, -0.128928),
-          initialZoom: 9.2,
-        ),
-        children: [
-          TileLayer(
-            urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-            userAgentPackageName: 'com.example.app',
-          ),
-          const RichAttributionWidget(
-            attributions: [
-              TextSourceAttribution(
-                'OpenStreetMap contributors',
+    return FutureBuilder(
+      // Utilisez FutureBuilder pour obtenir la position asynchrone.
+      future: _determinePosition(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          // Affichez un indicateur de chargement pendant que la position est récupérée.
+          return CircularProgressIndicator();
+        } else if (snapshot.hasError) {
+          // Gérez les erreurs liées à la récupération de la position.
+          return Center(child: Text('Erreur: ${snapshot.error}'));
+        } else {
+          // Si la position est récupérée avec succès, mettez à jour les options de la carte.
+          Position currentPosition = snapshot.data as Position;
+
+          return Scaffold(
+            appBar: AppBar(
+              title: const Text('Map Configuration'),
+            ),
+            body: FlutterMap(
+              options: MapOptions(
+                initialCenter: LatLng(currentPosition.latitude, currentPosition.longitude), // Mise à jour du centre de la carte.
+                initialZoom: 9.2,
               ),
-            ],
-          ),
-        ],
-      )
+              children: [
+                TileLayer(
+                  urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+                  userAgentPackageName: 'com.example.app',
+                ),
+                const RichAttributionWidget(
+                  attributions: [
+                    TextSourceAttribution(
+                      'OpenStreetMap contributors',
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          );
+        }
+      },
     );
   }
 }
