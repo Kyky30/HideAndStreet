7cdcc7fddc4f909a5b2e31f884ff8b5989c00159
Dorian Rochette <dorian.rochette@etu.uca.fr>
2024-01-15 10:18:48 +0100

    Fix de la disposition de la login page

--- lib/login.dart
+++ lib/login.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:hide_and_street/main.dart';
 import 'package:hide_and_street/password_forgoten.dart';
-import 'package:url_launcher/url_launcher.dart';
+// import 'package:url_launcher/url_launcher.dart';
 import 'package:figma_squircle/figma_squircle.dart';
 
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
@@ -13,16 +13,11 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body:
-        SingleChildScrollView(
-          child:
-      Stack(
-        children: [
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
+      body: SingleChildScrollView(
+        child: Container(
+          height: MediaQuery.of(context).size.height,
+          child: Stack(
             children: [
-              // Logo en haut de la page
               Align(
                 alignment: Alignment.topCenter,
                 child: Container(
@@ -35,118 +30,130 @@ class LoginPage extends StatelessWidget {
                         width: MediaQuery.of(context).size.width - 150,
                         fit: BoxFit.contain,
                       ),
-                      const SizedBox(height: 10),
                     ],
                   ),
                 ),
               ),
+              Positioned.fill(
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 0.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(15, 0, 15, 0),
+                        child: TextField(
+                          decoration: InputDecoration(
+                            filled: true,
+                            fillColor: Colors.grey[300],
+                            hintText: AppLocalizations.of(context)!.mail,
+                            border: OutlineInputBorder(
+                              borderSide: BorderSide.none,
+                              borderRadius: BorderRadius.circular(20.0),
+                            ),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 16),
 
-              const SizedBox(height: 16),
-
-              // Champ de texte pour le mail
-              Padding(
-                padding: const EdgeInsets.fromLTRB(15, 200, 15, 0),
-                child: TextField(
-                  decoration: InputDecoration(
-                    filled: true,
-                    fillColor: Colors.grey[300],
-                    hintText: AppLocalizations.of(context)!.mail,
-                    border: OutlineInputBorder(
-                      borderSide: BorderSide.none,
-                      borderRadius: BorderRadius.circular(20.0),
-                    ),
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(15, 8, 15, 0),
+                        child: TextField(
+                          obscureText: true,
+                          decoration: InputDecoration(
+                            filled: true,
+                            fillColor: Colors.grey[300],
+                            hintText: AppLocalizations.of(context)!.mdp,
+                            border: OutlineInputBorder(
+                              borderSide: BorderSide.none,
+                              borderRadius: BorderRadius.circular(20.0),
+                            ),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 16),
+                    ],
                   ),
                 ),
               ),
-              const SizedBox(height: 16),
-
-              // Champ de texte pour le mot de passe
-              Padding(
-                padding: const EdgeInsets.fromLTRB(15, 8, 15, 0),
-                child: TextField(
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    filled: true,
-                    fillColor: Colors.grey[300],
-                    hintText: AppLocalizations.of(context)!.mdp,
-                    border: OutlineInputBorder(
-                      borderSide: BorderSide.none,
-                      borderRadius: BorderRadius.circular(20.0),
-                    ),
-                  ),
-                ),
-              ),
-              const SizedBox(height: 16),
-
-              // Bouton pour naviguer vers la page room_joining.dart
-              Padding(
-                padding: const EdgeInsets.fromLTRB(15, 0, 15, 0),
-                child: ElevatedButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => MyHomePage(),
+              Positioned(
+                bottom: 20,
+                left: 0,
+                right: 0,
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(15, 0, 15, 0),
+                        child: ElevatedButton(
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => MyHomePage(),
+                              ),
+                            );
+                          },
+                          style: ElevatedButton.styleFrom(
+                            shape: SmoothRectangleBorder(
+                              borderRadius: SmoothBorderRadius(
+                                cornerRadius: 20,
+                                cornerSmoothing: 1,
+                              ),
+                            ),
+                            minimumSize: const Size(double.infinity, 80),
+                            backgroundColor: const Color(0xFF373967),
+                            foregroundColor: const Color(0xFF212348),
+                          ),
+                          child: Text(
+                            AppLocalizations.of(context)!.connexion,
+                            style: const TextStyle(fontSize: 20, fontWeight: FontWeight.w600, fontFamily: 'Poppins', color: Colors.white),
+                          ),
+                        ),
                       ),
-                    );
-                  },
-                  style: ElevatedButton.styleFrom(
-                    shape: SmoothRectangleBorder(
-                      borderRadius: SmoothBorderRadius(
-                        cornerRadius: 20,
-                        cornerSmoothing: 1,
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(15, 0, 15, 0),
+                        child: TextButton(
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => const ForgotenPassword(),
+                              ),
+                            );
+                          },
+                          child: Text(
+                              AppLocalizations.of(context)!.mdpoublie,
+                              style: const TextStyle(color: Colors.black, fontSize: 16, fontWeight: FontWeight.w600, fontFamily: 'Poppins', backgroundColor: Colors.white)
+                          ),
+                        ),
                       ),
-                    ),
-                    minimumSize: const Size(double.infinity, 80),
-                    backgroundColor: const Color(0xFF373967),
-                    foregroundColor: const Color(0xFF212348),
-                  ),
-                  child: Text(
-                    AppLocalizations.of(context)!.connexion,
-                    style: const TextStyle(fontSize: 20, fontWeight: FontWeight.w600, fontFamily: 'Poppins', color: Colors.white),
-                  ),
-                ),
-              ),
-              // Bouton "Mot de passe oublié ?"
-              Padding(
-                padding: const EdgeInsets.fromLTRB(15, 0, 15, 0),
-                child: TextButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => const ForgotenPassword(),
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(15, 0, 15, 0),
+                        child: TextButton(
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => RegisterPage(),
+                              ),
+                            );
+                          },
+                          child: Text(
+                              AppLocalizations.of(context)!.inscription,
+                              style: const TextStyle(color: Colors.black, fontSize: 16, fontWeight: FontWeight.w600, fontFamily: 'Poppins', backgroundColor: Colors.white)
+                          ),
+                        ),
                       ),
-                    );
-                  },
-                  child: Text(
-                      AppLocalizations.of(context)!.mdpoublie,
-                      style: const TextStyle(color: Colors.black, fontSize: 16, fontWeight: FontWeight.w600, fontFamily: 'Poppins', backgroundColor: Colors.white )
-                  ),
-                ),
-              ),
-              // Bouton "Mot de passe oublié ?"
-              Padding(
-                padding: const EdgeInsets.fromLTRB(15, 0, 15, 0),
-                child: TextButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => RegisterPage(),
-                      ),
-                    );                  },
-                  child: Text(
-                      AppLocalizations.of(context)!.inscription,
-                      style: const TextStyle(color: Colors.black, fontSize: 16, fontWeight: FontWeight.w600, fontFamily: 'Poppins', backgroundColor: Colors.white )
+                    ],
                   ),
                 ),
               ),
             ],
           ),
-        ],
-      ),
         ),
+      ),
     );
   }
 }
