c59e1e72ce823d400a8eac3fb07b2024536e0ff8
wilfrieddpt <120278338+wilfrieddpt@users.noreply.github.com>
2024-01-16 16:38:28 +0100

    Modification page premium + carrousel

--- lib/premium_page.dart
+++ lib/premium_page.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-import 'package:hide_and_street/squircle_block.dart';
 import 'package:figma_squircle/figma_squircle.dart';
 import 'package:purchases_flutter/purchases_flutter.dart';
-
+import 'package:carousel_slider/carousel_slider.dart';
 
 class PremiumPage extends StatelessWidget {
-  const PremiumPage({super.key});
+  const PremiumPage({Key? key});
+
 
   @override
   Widget build(BuildContext context) {
@@ -14,19 +14,79 @@ class PremiumPage extends StatelessWidget {
       appBar: AppBar(
         title: Text(AppLocalizations.of(context)!.infospremium),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            const SizedBox(height: 16),
-            SquircleInfoBlock(
-              title: AppLocalizations.of(context)!.avantagespremium,
-              informationList: [AppLocalizations.of(context)!.espacevide, AppLocalizations.of(context)!.nbparties, AppLocalizations.of(context)!.espacevide, AppLocalizations.of(context)!.publicite, AppLocalizations.of(context)!.espacevide, AppLocalizations.of(context)!.modesdejeutabprem, AppLocalizations.of(context)!.espacevide, AppLocalizations.of(context)!.reglagessupptabprem, AppLocalizations.of(context)!.espacevide, AppLocalizations.of(context)!.cosmetiquesuppmoistabprem, AppLocalizations.of(context)!.espacevide, AppLocalizations.of(context)!.cosmetiquesupptabprem],
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Text(
+              AppLocalizations.of(context)!.avantagespremium,
+              style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.black),
             ),
-            const SizedBox(height: 16),
-            ElevatedButton(
+          ),
+         Container(
+           decoration: BoxDecoration(
+             borderRadius: SmoothBorderRadius(
+             cornerRadius: 20,
+             cornerSmoothing: 1,
+            ),
+             color: Colors.amber,
+
+           ),
+           width: (MediaQuery.of(context).size.width)*0.78,
+
+           child:  CarouselSlider(
+             options: CarouselOptions(
+               height: MediaQuery.of(context).size.height * 0.65,
+               enableInfiniteScroll: true,
+               enlargeCenterPage: true,
+
+             ),
+             items: [
+               _buildCarouselItem(
+                 context,
+                 title: AppLocalizations.of(context)!.nbparties,
+                 description: AppLocalizations.of(context)!.espacevide,
+                 image: AssetImage("assets/premium.png"),
+               ),
+               _buildCarouselItem(
+                 context,
+                 title: AppLocalizations.of(context)!.publicite,
+                 description: AppLocalizations.of(context)!.espacevide,
+                 image: AssetImage("assets/premium.png"),
+               ),
+               _buildCarouselItem(
+                 context,
+                 title: AppLocalizations.of(context)!.modesdejeutabprem,
+                 description: AppLocalizations.of(context)!.espacevide,
+                 image: AssetImage("assets/premium.png"),
+               ),
+               _buildCarouselItem(
+                 context,
+                 title: AppLocalizations.of(context)!.reglagessupptabprem,
+                 description: AppLocalizations.of(context)!.espacevide,
+                 image: AssetImage("assets/premium.png"),
+               ),
+               _buildCarouselItem(
+                 context,
+                 title: AppLocalizations.of(context)!.cosmetiquesuppmoistabprem,
+                 description: AppLocalizations.of(context)!.espacevide,
+                 image: AssetImage("assets/premium.png"),
+               ),
+               _buildCarouselItem(
+                 context,
+                 title: AppLocalizations.of(context)!.cosmetiquesupptabprem,
+                 description: AppLocalizations.of(context)!.espacevide,
+                 image: AssetImage("assets/premium.png"),
+               ),
+             ],
+           ),
+         ),
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: ElevatedButton(
+
               onPressed: () async {
-                await Purchases.purchaseStoreProduct('premium_subscription' as StoreProduct); // Correction d'un warning pouvant poser probléme
+                await Purchases.purchaseStoreProduct('premium_subscription' as StoreProduct);
               },
               style: ElevatedButton.styleFrom(
                 shape: SmoothRectangleBorder(
@@ -35,19 +95,50 @@ class PremiumPage extends StatelessWidget {
                     cornerSmoothing: 1,
                   ),
                 ),
+
                 minimumSize: const Size(double.infinity, 80),
-                backgroundColor: const Color(0xFF373967),
-                foregroundColor: const Color(0xFF212348),
+                backgroundColor: const Color(0xFFDAA520),
+                foregroundColor: const Color(0xFF000000),
+
               ),
               child: Text(
                 AppLocalizations.of(context)!.acheterpremium,
                 style: const TextStyle(fontSize: 20, fontWeight: FontWeight.w600, fontFamily: 'Poppins', color: Colors.white),
               ),
             ),
-          ],
-        ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildCarouselItem(
+      BuildContext context, {
+        required String title,
+        required String description,
+        required AssetImage image,
+      }) {
+    return Container(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            title,
+            style: const TextStyle(fontSize: 14, fontWeight: FontWeight.bold, color: Colors.black),
+          ),
+          const SizedBox(height: 16),
+          ClipRRect(
+            borderRadius: BorderRadius.circular(20),
+            child: Image(
+              image: image,
+              fit: BoxFit.cover,
+              width: double.infinity,
+            ),
+          ),
+          const SizedBox(height: 16),
+        ],
       ),
     );
   }
 }
-
